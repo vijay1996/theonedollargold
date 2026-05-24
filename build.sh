cdc90e7d8f7cddb305471e4a9331790a0929b878
vijay1996 <vijaybhojraj.cm@gmail.com>
2026-05-24 21:46:03 +0530

    added railway.toml

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export VITE_SUPABASE_URL="${VITE_SUPABASE_URL}"
-export VITE_SUPABASE_ANON_KEY="${VITE_SUPABASE_ANON_KEY}"
-vite build
-esbuild server.ts --bundle --platform=node --format=cjs --packages=external --sourcemap --outfile=dist/server.cjs
