8cdd7b1afcf9ebfee2f30284575480734a2c6c0f
vijay1996 <vijaybhojraj.cm@gmail.com>
2026-05-28 16:32:20 +0530

    corrected adsense code

--- src/AdSense.tsx
+++ src/AdSense.tsx
@@ -8,7 +8,8 @@ export default function Header() {
     getUserSubscriptionInfo().then((info) => {
       setSubInfo(info);
     });
+    console.log('Header mounted, checking subscription info');
   }, []);
 
-  return isPremium(subInfo?.tier, subInfo?.status) ? <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6748935947885860" crossOrigin="anonymous"></script> : null; 
+  return !isPremium(subInfo?.tier, subInfo?.status) ? <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6748935947885860" crossOrigin="anonymous"></script> : null; 
 }
