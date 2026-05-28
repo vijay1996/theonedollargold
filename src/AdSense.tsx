4298f694c2a9c639dd57f033f471e9081a9e3479
vijay1996 <vijaybhojraj.cm@gmail.com>
2026-05-28 16:11:55 +0530

    updated google adsense tag

---
+++ src/AdSense.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { getUserSubscriptionInfo, isPremium } from "./lib/razorpay";
+
+export default function Header() {
+  const [subInfo, setSubInfo] = useState<any>(null);
+
+  useEffect(() => {
+    getUserSubscriptionInfo().then((info) => {
+      setSubInfo(info);
+    });
+  }, []);
+
+  return isPremium(subInfo?.tier, subInfo?.status) ? <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6748935947885860" crossOrigin="anonymous"></script> : null; 
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import Header from './Header.tsx';
+import AdSense from './AdSense.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
     <App />
+    <AdSense />
   </StrictMode>,
 );
